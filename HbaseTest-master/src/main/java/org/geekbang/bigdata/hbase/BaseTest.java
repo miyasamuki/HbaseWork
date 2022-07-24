9c53fc99871b656fd5d618012dc7c6c1c73946a3
gonghanqi <137264520@qq.com>
2022-07-24 23:28:44 +0800

    Initial commit

---
+++ HbaseTest-master/src/main/java/org/geekbang/bigdata/hbase/BaseTest.java
@@ -0,0 +1,69 @@
+package org.geekbang.bigdata.hbase;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+
+public class BaseTest {
+
+    public static void main(String[] args) throws IOException {
+        System.setProperty("hadoop.home.dir","D:\\BigData");
+        // 建立连接
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.quorum", "127.0.0.1");
+        configuration.set("hbase.zookeeper.property.clientPort", "2181");
+        configuration.set("hbase.master", "127.0.0.1:60000");
+        Connection conn = ConnectionFactory.createConnection(configuration);
+        Admin admin = conn.getAdmin();
+
+        TableName tableName = TableName.valueOf("ghq:student");
+        String colFamily = "info";
+        int rowKey = 1;
+
+        // 建表
+        if (admin.tableExists(tableName)) {
+            System.out.println("Table already exists");
+        } else {
+            HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
+            HColumnDescriptor hColumnDescriptor = new HColumnDescriptor(colFamily);
+            hTableDescriptor.addFamily(hColumnDescriptor);
+            admin.createTable(hTableDescriptor);
+            System.out.println("Table create successful");
+        }
+
+        // 插入数据
+        Put put = new Put(Bytes.toBytes(rowKey)); // row key
+        put.addColumn(Bytes.toBytes(colFamily), Bytes.toBytes(""), Bytes.toBytes("string")); // col1
+        put.addColumn(Bytes.toBytes(colFamily), Bytes.toBytes("name"), Bytes.toBytes("Tom")); // col2
+        conn.getTable(tableName).put(put);
+        System.out.println("Data insert success");
+
+        // 查看数据
+        Get get = new Get(Bytes.toBytes(rowKey));
+        if (!get.isCheckExistenceOnly()) {
+            Result result = conn.getTable(tableName).get(get);
+            for (Cell cell : result.rawCells()) {
+                String colName = Bytes.toString(cell.getQualifierArray(), cell.getQualifierOffset(), cell.getQualifierLength());
+                String value = Bytes.toString(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength());
+                System.out.println("Data get success, colName: " + colName + ", value: " + value);
+            }
+        }
+
+        // 删除数据
+        Delete delete = new Delete(Bytes.toBytes(rowKey));      // 指定rowKey
+        conn.getTable(tableName).delete(delete);
+        System.out.println("Delete Success");
+
+        // 删除表
+        if (admin.tableExists(tableName)) {
+            admin.disableTable(tableName);
+            admin.deleteTable(tableName);
+            System.out.println("Table Delete Successful");
+        } else {
+            System.out.println("Table does not exist!");
+        }
+    }
+}
